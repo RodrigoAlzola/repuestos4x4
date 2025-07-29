14c883dd14f6f675f1127004236fcc74e5063399
Rodrigo Alzola <ralzola@miuandes.cl>
2025-07-29 00:51:29 -0400

    changes form dev and prod 7

--- ecom/settings/prod.py
+++ ecom/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['https://repuesto4x4.com', 'repuesto4x4.com', 'repuestos4x4-production.up.railway.app', 'https://repuestos4x4-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://repuesto4x4.com', 'https://repuestos4x4-production.up.railway.app']
