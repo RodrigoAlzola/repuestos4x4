dac6bfe94f355c72a325717327551cdc8a2df2be
Rodrigo Alzola <ralzola@miuandes.cl>
2025-12-13 22:17:40 -0300

    email timeout

--- ecom/settings/prod.py
+++ ecom/settings/prod.py
@@ -41,7 +41,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@4x4max.cl')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_TIMEOUT = 30  # Timeout de 10 segundos
 
 # Seguridad para producción
