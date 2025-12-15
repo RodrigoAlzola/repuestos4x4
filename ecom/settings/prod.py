97e6d3fc61967bc1b6dd4bf7fe18a3da7a3de8cd
Rodrigo Alzola <ralzola@miuandes.cl>
2025-12-15 00:56:26 -0300

    email port

--- ecom/settings/prod.py
+++ ecom/settings/prod.py
@@ -37,7 +37,7 @@ TRANSBANK_API_KEY = os.environ.get('PROD_TRANSBANK_API_KEY')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.zoho.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
