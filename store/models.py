ac65a6fe04d986c5f673bfa7b1f7fec770417980
Rodrigo Alzola <ralzola@miuandes.cl>
2026-01-07 18:26:43 -0300

    Old Cart incremento de caracteres 2

--- store/models.py
+++ store/models.py
@@ -17,7 +17,7 @@ class Profile(models.Model):
     commune = models.CharField(max_length=200, blank=True)
     zipcode = models.CharField(max_length=200, blank=True)
     country = models.CharField(max_length=200, blank=True)
-    old_cart = models.CharField(max_length=20000, blank=True)
+    old_cart = models.TextField(blank=True, null=True, default='')
 
     def __str__(self):
         return self.user.username
